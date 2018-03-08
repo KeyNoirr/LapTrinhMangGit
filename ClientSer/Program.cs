08c4a560b87ac0ff1b659217fb5eb1b045d015fc
TRAN CAO SON <15dh110206@st.huflit.edu.vn>
2018-03-08 21:55:44 +0700

    Add project files.

---
+++ ClientSer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientSer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            client.Connect(ipServer);
+            string sGui = "Hello Server!";
+            byte[] gui = Encoding.ASCII.GetBytes(sGui);
+            client.Send(gui);
+            byte[] nhan = new byte[1024];
+            int rec = client.Receive(nhan);
+            string sNhan = Encoding.ASCII.GetString(nhan, 0, rec);
+            Console.WriteLine("Server gui: {0}", sNhan);
+            client.Close();
+        }
+    }
+}
---
+++ ServerCli/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ServerCli
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+
+            server.Bind(ip);
+            server.Listen(3);
+            Socket client = server.Accept();
+            byte[] nhan = new byte[1024];
+            int rec = client.Receive(nhan);
+            string sNhan = Encoding.ASCII.GetString(nhan, 0, rec);
+            Console.WriteLine("Client gui: {0}", sNhan);
+
+            string sGui = "Hello Client!";
+            byte[] gui = Encoding.ASCII.GetBytes(sGui);
+            client.Send(gui);
+            client.Close();
+        }
+    }
+}
